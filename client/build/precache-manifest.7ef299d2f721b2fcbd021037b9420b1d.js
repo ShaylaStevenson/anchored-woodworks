1de1a5aef8a437dd0994336a129ae2c1171123e7
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-07-31 16:27:48 -0700

    deploy to gh-pages

---
+++ client/build/precache-manifest.7ef299d2f721b2fcbd021037b9420b1d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0892db79b0f12dba1f46",
+    "url": "/anchored-woodworks/static/css/main.b9582638.chunk.css"
+  },
+  {
+    "revision": "0892db79b0f12dba1f46",
+    "url": "/anchored-woodworks/static/js/main.9352eff1.chunk.js"
+  },
+  {
+    "revision": "fa2198abb6d5c161cf18",
+    "url": "/anchored-woodworks/static/js/runtime~main.8d9ac6cf.js"
+  },
+  {
+    "revision": "260308a560e5ea839e6f",
+    "url": "/anchored-woodworks/static/css/2.e73dfa0a.chunk.css"
+  },
+  {
+    "revision": "260308a560e5ea839e6f",
+    "url": "/anchored-woodworks/static/js/2.d278efa8.chunk.js"
+  },
+  {
+    "revision": "e1410516a61829ecd3eeb7387f0fe711",
+    "url": "/anchored-woodworks/static/media/tools.e1410516.jpg"
+  },
+  {
+    "revision": "a49b22b8483255e09f418806b126ee7a",
+    "url": "/anchored-woodworks/static/media/png-tree-bulb.a49b22b8.png"
+  },
+  {
+    "revision": "4fc38738f924f5b9d6757599a6215728",
+    "url": "/anchored-woodworks/static/media/wood-round.4fc38738.jpg"
+  },
+  {
+    "revision": "f4c1651b2449cf70d490877c7ca87e00",
+    "url": "/anchored-woodworks/static/media/creativeDesignScreen.f4c1651b.png"
+  },
+  {
+    "revision": "7b8dc0e037b6c5ad377b1a667810a6d8",
+    "url": "/anchored-woodworks/static/media/cabinet-transparent.7b8dc0e0.png"
+  },
+  {
+    "revision": "36169e0e024534b20d8b8f5a2c1bc35a",
+    "url": "/anchored-woodworks/static/media/brown.36169e0e.png"
+  },
+  {
+    "revision": "d29db85ec941192fddad59c5650c1766",
+    "url": "/anchored-woodworks/index.html"
+  }
+];
